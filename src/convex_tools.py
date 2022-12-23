567c8d45b1c1813d2e8653ba89a7d8db95c155ce
Vivano <armandviolle@gmail.com>
2022-12-23 12:48:49 +0100

    correction d'une erreur d'indexation dans la projection sur le simplex

--- src/convex_tools.py
+++ src/convex_tools.py
@@ -7,18 +7,18 @@ def simplex_proj(x):
 	if np.sum(x) == 1:
 		return x
 	else:
-		print(x.shape)
+		# print(x.shape)
 		x = np.sort(x)[::-1]
 		i = 1
 		while ( x[i-1] - (np.sum(x[:i]) - 1)/i ) > 0:
-			i +=1
-		d = i
+			i += 1
+		d = i-1
 		theta = (np.sum(x[:d+1]) - 1) / d
 		return SoftThreshold(x, theta)
 
 def l1_ball_projection(z, x):
 	if np.linalg.norm(x) <= z:
 		return x
-	else:	
-		proj = simplex_proj(np.sign(x) * x / z)
+	else:		
+		proj = simplex_proj(abs(x) / z)
 		return np.sign(x) * proj
