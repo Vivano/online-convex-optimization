4e57232546df6bf2625cffbea4032abe31a063cc
Sobihan18 <108264726+Sobihan18@users.noreply.github.com>
2022-12-23 21:36:19 +0100

    Update convex_tools.py

--- src/convex_tools.py
+++ src/convex_tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cvxpy as cp
 
 def SoftThreshold(x, y):
 	return np.maximum(x-y, 0)
@@ -25,3 +26,34 @@ def l1_ball_projection(z, x):
 	else:		
 		proj = simplex_proj(abs(x) / z)
 		return np.sign(x) * z * proj 
+
+	
+def proj_simplex_w(x, D):
+	dx = D @ x
+	dxx = np.sort(dx)[::-1]
+	dinv = 1/np.diag(D)
+	term = dxx - (1/np.cumsum(dinv))*(np.cumsum(x) -1) 
+	
+	d0 = np.argmax(term) + 1
+	theta = (1/np.sum(dinv[:d0]))*(np.sum(x[:d0]) - 1)
+	return np.linalg.inv(D) @ SoftThreshold(dx, theta)
+
+
+def generalized_projection(x, D, z=1):
+	if np.sum(np.abs(x)) <= z:
+		return x
+	else:
+		proj = proj_simplex_w(abs(x) / z, D)
+	return np.sign(x) * z * proj
+
+
+def generalized_projection2(y, W, z):   # page 41
+    d = y.shape[0]
+    x = cp.Variable(d)
+    objective = cp.Minimize(cp.matrix_frac(y - x, W)) # min (y-x) W^-1 (y-x)
+    constraint = [cp.norm(x, 1) <= z] # s.t. l1 norm(y) <= z
+    prob = cp.Problem(objective, constraint)
+    result = prob.solve()
+    if result != cp.OPTIMAL:
+        print(f'Generalized projection error: optimization is not optimal and ends with status {result}')
+    return x.value
